72eae6bfae088d46e25aa900395b682c0c3ab3b6
Kyle Jones <kylej@twitter.com>
2022-05-30 22:13:56 +0100

    Initial commit - proxy server written

---
+++ proxy-server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// Configuration
+const PORT = 3000;
+const HOST = 'localhost';
+const API_SERVICE_URL = 'https://itunes.apple.com/search';
+
+// Info GET endpoint
+app.get('/info', (req, res, next) => {
+  res.send(
+    'This is a proxy service which should connect to Apple Music service.'
+  );
+});
+
+// Proxy endpoints
+app.use(
+  '/search',
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      '^/json_placeholder': ''
+    }
+  })
+);
+
+// Start the Proxy
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
